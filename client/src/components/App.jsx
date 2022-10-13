a69d2252c3e3eed09be670b75e14448bd9d27e16
Jadennnnnnn <jadenjee@gmail.com>
2022-10-13 15:02:15 -0700

    work in progress

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     <div>
       hello world
       <button onClick={() => temp()}>click</button>
-      <RelatedItemsAndComparison {/*currentItem={currentItem}*/}/>
+      <RelatedItemsAndComparison />
     </div>
   )
 }
