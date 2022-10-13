146ea4f2937061a051acd680d8624e56da614bd2
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2022-10-13 14:43:26 -0700

    refactor controllers/models, start state in App.jsx

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,9 @@
 const controllers = require('./controllers/index.js')
 const router = require('express').Router()
 
-// router.get('/', controllers.function)
 router.get('/products', controllers.products.getAll)
-// router.get('/products/:id', controllers.products.getOne)
+router.get('/products/:id', controllers.products.getOne)
+router.get('/products/:id/styles', controllers.products.getStyles)
 
-// router.get('/afjalsflsdkfj', controllers.QnA.getAll)
 
 module.exports = router
