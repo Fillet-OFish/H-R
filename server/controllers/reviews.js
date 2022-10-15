75d2003fa42cc72504d5eb110fdf26a3eebd0d9b
Jadennnnnnn <jadenjee@gmail.com>
2022-10-14 18:39:42 -0700

    merge

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -4,10 +4,6 @@ module.exports = {
   getAll: function (req, res) {
     models.reviews.getAll(req.params.id)
       .then(result => {res.status(200).send(result.data)})
-<<<<<<< HEAD
       .catch(err => console.log(err));
-=======
-      .catch(err => console.log('reviews getAll error', err.response.data));
->>>>>>> master
   },
 }
