2d4487011baffe99496b7ef620f52a621d97818a
Jadennnnnnn <jadenjee@gmail.com>
2022-10-17 11:36:16 -0700

    star rating shows stars filled in. need to do 1/4 fills

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -4,28 +4,28 @@ module.exports = {
   getAll: function (req, res) {
     models.products.getProducts()
       .then(result => {res.status(200).send(result.data)})
-      .catch(e => console.log('products getAll error', e.response.data))
+      .catch(e => console.log('products getAll error', e))
   },
   getOne: function (req, res) {
     models.products.getProduct(req.params.id)
       .then(result => res.status(200).send(result.data))
-      .catch(e => console.log('products getOne error', e.response.data))
+      .catch(e => console.log('products getOne error', e))
 
   },
   getStyles: function (req, res) {
     models.products.getStyles(req.params.id)
       .then(result => res.status(200).send(result.data))
-      .catch(e => console.log('products getStyles error', e.response.data))
+      .catch(e => console.log('products getStyles error', e))
 
   },
   getRelated: function (req, res) {
     models.products.getRelated(req.params.id)
       .then(result => res.status(200).send(result.data))
-      .catch(e => console.log('products getRelated error', e.response.data))
+      .catch(e => console.log('products getRelated error', e))
   },
   product: function (req, res) {
     models.products.getProduct(req.params.id)
       .then(result => res.status(200).send(result.data))
-      .catch(e => console.log('products prouct error', e.response.data))
+      .catch(e => console.log('products prouct error', e))
   }
 }
