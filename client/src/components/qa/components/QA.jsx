577b639eb642268850d2654cef8fd8b5da099562
Fernando Gamboa <103979716+Fernando-Gamboa@users.noreply.github.com>
2022-10-27 15:27:33 -0700

    merged fixed small bugs (#80)

--- client/src/components/qa/components/QA.jsx
+++ client/src/components/qa/components/QA.jsx
@@ -60,7 +60,7 @@ const QA = (props) => {
       <div id='qaContainer' className='QContainer'>
         {/* set each question entry --------- */}
         {currentRecords.map((ques, index) => (
-          <QAEntry ques={ques} key={index} index={index} lastIndex={currentRecords.length - 1} qaData={props.qaData} setImage={props.setImage} setModalOn={props.setModalOn} modalOn={props.modalOn} modalAnswOn={props.modalAnswOn} setModalAnswOn={props.setModalAnswOn} setQID={props.setQID} />
+          <QAEntry ques={ques} key={index} index={index} lastIndex={currentRecords.length - 1} qaData={props.qaData} setImage={props.setImage} setModalOn={props.setModalOn} modalOn={props.modalOn} modalAnswOn={props.modalAnswOn} setModalAnswOn={props.setModalAnswOn} setQID={props.setQID} fetch={fetch} setFetch={setFetch} />
           ))}
       </div>
 
